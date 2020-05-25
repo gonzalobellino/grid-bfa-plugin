24933af2d7bfc1088f0255754ea285540c76d419
gonzalobellino <nomorealias@x-pc.localdomain>
2020-05-24 21:01:27 -0300

    Plugin for running in testnet environment a node over Blockchain Federal Argentina (BFA). For this plugin works, its required a pre initialized database with a genesis.json for the network

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-let platform = 'windows'
-let dataDir = `${process.env.APPDATA}/Ethereum`
+const path = require('path');
+const fs = require('fs-extra')
+
+let  platform = 'windows'
+let  networkEnv = 'testnet'
+let  bfaDir = 'BFA' + path.sep + networkEnv
+let  dataDir = `${process.env.APPDATA}/` + bfaDir
+
+let bootnodes =  "enode://7ec4dd9d5e1a2b29d6b60aa9f95677c0c3a5f9306e73d65dd3bcbfda3737a8c509b02d1eab763ce39f18cfe96423df7ee544d6c36191ec17f59ade75bc99d358@[2800:110:44:6300::aad2:2db3]:30301," +
+                 "enode://7ec4dd9d5e1a2b29d6b60aa9f95677c0c3a5f9306e73d65dd3bcbfda3737a8c509b02d1eab763ce39f18cfe96423df7ee544d6c36191ec17f59ade75bc99d358@170.210.45.179:30301," +
+                 "enode://82b66b13d7addcf9ffe1e4e972a105f6ccf50557161c4a0978a5d9ce595ababde609ea8a49897ae89b1d41e90551cb2e9241363238593e950ca68bd5af7c24d6@200.16.28.28:30301," +
+                 "enode://59ae768ecdee632e0daceccb6f71b215392eba89230d626573f2fb4e9c0786c9a661027ab7343820ca63d96fe48ffd81ed1bf6e4d512f0ba50ec072c9efd9e4e@200.108.146.100:30301"
 
 // Platform specific initialization
 switch (process.platform) {
   case 'win32': {
     platform = 'windows'
-    dataDir = `${process.env.APPDATA}\\Ethereum`
+    dataDir = `${process.env.APPDATA}\\` + bfaDir
     break
   }
   case 'linux': {
     platform = 'linux'
-    dataDir = '~/.ethereum'
+    dataDir = '~/.' + bfaDir
     break
   }
   case 'darwin': {
     platform = 'darwin'
-    dataDir = '~/Library/Ethereum'
+    dataDir = '~/Library/' + bfaDir
     break
   }
   default: {
   }
 }
+
+console.log("copy database to " + dataDir)
+fs.mkdirSync(dataDir, {recursive:true})
+fs.copySync(path.join(__dirname, 'geth'),path.join(dataDir, 'geth'),
+  {overwrite : false})
 
 const keystoreDir =
   process.platform === 'win32' ? `${dataDir}\\keystore` : `${dataDir}/keystore`
@@ -46,8 +61,8 @@ const findIpcPathInLogs = logs => {
 module.exports = {
   type: 'client',
   order: 1,
-  displayName: 'BFA-geth',
-  name: 'bfa-geth',
+  displayName: 'BFA Testnet',
+  name: 'BFA.testnet',
   repository: 'https://gethstore.blob.core.windows.net',
   modifiers: {
     version: ({ version }) =>
@@ -62,22 +77,31 @@ module.exports = {
       excludes: ['unstable', 'alltools', 'swarm', 'mips', 'arm']
     }
   },
-  prefix: `bfa-geth-${platform}`,
+  prefix: `geth-${platform}`,
   binaryName: process.platform === 'win32' ? 'geth.exe' : 'geth',
   resolveIpc: logs => findIpcPathInLogs(logs),
   settings: [
     {
-      id: 'network',
-      default: 'main',
-      label: 'Network',
-      options: [
-        { value: 'testnet', label: 'Ropsten (testnet)', flag: '--testnet' },
-        { value: 'mainnet', label: 'Rinkeby (testnet)', flag: '--rinkeby' }
-      ]
+      id: bootnodes,
+      default: bootnodes,
+      label: 'bootnodes',
+      flag: '--bootnodes %s'
+    },
+    {
+      id: 'networkId',
+      default: '55555000000',
+      label: 'Network Id',
+      flag: '--networkid %s'
+    },
+    {
+      id: 'dataDir',
+      default: dataDir,
+      label: 'Data Directory',
+      flag: '--datadir %s'
     },
     {
       id: 'syncMode',
-      default: 'light',
+      default: 'fast',
       label: 'Sync Mode',
       options: [
         { value: 'fast', label: 'Fast' },
@@ -90,15 +114,19 @@ module.exports = {
       id: 'dataDir',
       default: dataDir,
       label: 'Data Directory',
-      flag: '--datadir %s',
-      type: 'directory'
+      options: [
+        { value: dataDir, label: dataDir }
+      ],
+       flag: '--datadir %s'
     },
     {
       id: 'keystoreDir',
       default: keystoreDir,
       label: 'Keystore Directory',
+      options: [
+        { value: keystoreDir, label: keystoreDir }
+      ],
       flag: '--keystore %s',
-      type: 'directory'
     },
     {
       id: 'console',
@@ -111,7 +139,7 @@ module.exports = {
     },
     {
       id: 'rpc',
-      default: 'none',
+      default: 'on',
       label: 'HTTP RPC API',
       options: [
         { value: 'none', label: 'Disabled', flag: '' },
@@ -151,7 +179,7 @@ module.exports = {
     {
       id: 'graphql',
       label: 'Enable GraphQL Server',
-      default: 'false',
+      default: 'true',
       options: [
         {
           value: 'true',
@@ -195,62 +223,67 @@ module.exports = {
         { value: 4, label: '4 = Debug', flag: '--loglevel=4' },
         { value: 5, label: '5 = Detail', flag: '--loglevel=5' }
       ]
-    }
+    },
+    {
+      id: 'ipcdisabled',
+      label: 'IPC Disabled',
+      default: 'true',
+      options: [
+        { value: 'true', flag: '--ipcdisable', label: 'Yes' },
+      ]
+    },
   ],
   about: {
     description:
-      'Geth is a multipurpose command line tool that runs a full Ethereum node implemented in Go.',
+      'Blockchain Federal Argentina (BFA) es una plataforma multiservicios abierta y participativa pensada para integrar servicios y aplicaciones sobre blockchain',
     apps: [
       {
         name: 'Block Explorer',
         url: 'package://github.com/marcgarreau/grid-blocks-app',
         dependencies: [
           {
-            name: 'geth',
+            name: 'BFA.testnet',
             settings: [{ id: 'graphql', value: 'true' }]
           }
         ]
       },
       {
-        name: 'RPC Tester App',
-        url: 'package://github.com/ryanio/grid-rpc-app',
+        name: 'GraphQL App',
+        url: 'http://localhost:8547',
         dependencies: [
           {
-            name: 'geth',
-            settings: []
+            name: 'BFA.testnet',
+            settings: [{ id: 'graphql', value: 'true' }]
           }
         ]
-      },
-      {
-        name: 'GraphQL App',
-        url: 'http://localhost:8547'
+
       }
     ],
     links: [
       {
-        name: 'GitHub Repository',
-        url: 'https://github.com/ethereum/go-ethereum'
+        name: 'Gitlab Repository',
+        url: 'https://gitlab.bfa.ar/public'
       }
     ],
     docs: [
       {
-        name: 'Geth Docs',
-        url: 'https://geth.ethereum.org/docs/'
+        name: 'Official Site',
+        url: 'https://bfa.ar/'
       },
       {
-        name: 'Geth Changelog',
-        url: 'https://github.com/ethereum/go-ethereum/releases'
+        name: 'Wiki',
+        url: 'https://gitlab.bfa.ar/blockchain/docs/wikis/home'
       },
       {
-        name: 'JSON RPC API Reference',
+        name: 'Mailing List',
         url:
-          'https://github.com/ethereum/wiki/wiki/JSON-RPC#json-rpc-api-reference'
+          'https://listas.bfa.ar/listinfo'
       }
     ],
     community: [
       {
-        name: 'Discord Chat',
-        url: 'https://discordapp.com/invite/nthXNEv'
+        name: 'Telegram Chat',
+        url: 'https://t.me/bfatec'
       }
     ]
   }
